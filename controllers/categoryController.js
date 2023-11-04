9b592ed4a6f483d881c991636d8f4e919140d2d3
SynthCyraX <SynthCyraX@gmail.com>
2023-11-04 06:26:14 +0100

    Added placeholder in categoryController

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -35,3 +35,27 @@ exports.category_detail = asyncHandler(async (req, res, next) => {
     category_items: itemsInCategory,
   });
 });
+// Display Category create form on GET.
+exports.category_create_get = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: Category create GET');
+});
+// Handle Category create form on POST.
+exports.category_create_post = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: Category create POST');
+});
+// Display Category delete form on GET
+exports.category_delete_get = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: Category delete GET');
+});
+// Handle Category delete form on POST.
+exports.category_delete_post = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: Category delete POST');
+});
+// Display Category update form on GET
+exports.category_update_get = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: Category update GET');
+});
+// Handle Category update form on POST.
+exports.category_update_post = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: Category update POST');
+});
