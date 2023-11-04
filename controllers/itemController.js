4c9137abf659eef5835323e88abae645a1b3beea
SynthCyraX <SynthCyraX@gmail.com>
2023-11-04 08:45:06 +0100

    Added basic views

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -7,7 +7,17 @@ const asyncHandler = require('express-async-handler');
 
 // Display index
 exports.index = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Index ');
+  // Get details of books, book instances, authors and genre counts (in parallel)
+  const [numItems, numCategories] = await Promise.all([
+    Item.countDocuments({}).exec(),
+    Category.countDocuments({}).exec(),
+  ]);
+
+  res.render('index', {
+    title: 'Inventory Application Home',
+    item_count: numItems,
+    category_count: numCategories,
+  });
 });
 // Display all items
 exports.item_list = asyncHandler(async (req, res, next) => {
