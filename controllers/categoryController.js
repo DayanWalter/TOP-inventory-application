6ac6414cf04c5389bb80e0f45b68b0f9765fb261
SynthCyraX <SynthCyraX@gmail.com>
2023-11-04 06:19:13 +0100

    Added display detail/all in itemController

---
+++ controllers/categoryController.js
@@ -0,0 +1,37 @@
+const Category = require('../models/category');
+const Item = require('../models/item');
+
+const { body, validationResult } = require('express-validator');
+
+const asyncHandler = require('express-async-handler');
+
+// Display list of all categories
+exports.category_list = asyncHandler(async (req, res, next) => {
+  const allCategory = await Category.find().sort({ name: 1 }).exec();
+  res.render('category_list', {
+    title: 'Category List',
+    category_list: allCategory,
+  });
+});
+
+// Display detail page for a specific category
+exports.category_detail = asyncHandler(async (req, res, next) => {
+  // Get detail of category and all associated items(in parallel)
+  const [category, itemsInCategory] = await Promise.all([
+    Category.findById(req.params.id).exec(),
+    Item.find({ category: req.params.id }, 'name description').exec(),
+  ]);
+
+  // Throw error if category is not found
+  if (category === null) {
+    const err = new Error('Category not found');
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render('category_detail', {
+    title: 'Category Detail',
+    category,
+    category_items: itemsInCategory,
+  });
+});
